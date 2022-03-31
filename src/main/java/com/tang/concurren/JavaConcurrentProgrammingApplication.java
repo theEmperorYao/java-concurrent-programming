7baf96aa8832bef73a7ff5a479d1e3dfd6a026bb
tangyao <1013654193@qq.com>
2022-03-31 11:42:32 +0800

    第一次提交

---
+++ src/main/java/com/tang/concurren/JavaConcurrentProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.tang.concurren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcurrentProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaConcurrentProgrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tang/concurren/JavaConcurrentProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tang.concurren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConcurrentProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
